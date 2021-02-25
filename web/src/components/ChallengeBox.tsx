dcd631ef3752961afc346bb8b93ba0f73a499ed8
Rafael F. Silva <rafaelfsilva1@gmail.com>
2021-02-25 10:30:19 -0300

    fourth class 2020-02-25

--- web/src/components/ChallengeBox.tsx
+++ web/src/components/ChallengeBox.tsx
@@ -1,12 +1,20 @@
 import { useContext } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownContext } from '../contexts/CountdownContext';
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox () {
-  const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
+  const { activeChallenge, resetChallenge, completeChallenge } = useContext(ChallengesContext);
+  const { resetCountdown } = useContext(CountdownContext);
 
-  function complete () {
+  function handleChallengeSucceded () {
+    completeChallenge();
+    resetCountdown();
+  }
 
+  function handleChallengeFailed () {
+    resetChallenge();
+    resetCountdown();
   }
 
   return (
@@ -24,14 +32,14 @@ export function ChallengeBox () {
               <button
                 type="button"
                 className={styles.challengeFailedButton}
-                onClick={resetChallenge}
+                onClick={handleChallengeFailed}
               >
                 Falhei
               </button>
               <button
                 type="button"
                 className={styles.challengeSuccededButton}
-                onClick={complete}
+                onClick={handleChallengeSucceded}
               >
                 Completei
               </button>
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -5,6 +5,7 @@ import { Profile } from '../components/Profile';
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
 import { ChallengeBox } from '../components/ChallengeBox';
+import { CountdownProvider } from '../contexts/CountdownContext';
 import styles from '../styles/pages/Home.module.css';
 
 export default function Home () {
@@ -16,17 +17,19 @@ export default function Home () {
 
       <ExperienceBar />
 
-      <section>
-        <div>
-          <Profile />
-          <CompletedChallenges />
-          <Countdown />
-        </div>
+      <CountdownProvider>
+        <section>
+          <div>
+            <Profile />
+            <CompletedChallenges />
+            <Countdown />
+          </div>
 
-        <div>
-          <ChallengeBox />
-        </div>
-      </section>
+          <div>
+            <ChallengeBox />
+          </div>
+        </section>
+      </CountdownProvider>
     </div>
   );
 }
